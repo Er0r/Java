f20684bc883b94767840915810f44805783aa06e
Er0r <47925596+Er0r@users.noreply.github.com>
2019-03-11 20:13:21 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraynew.udemy;
+
+/**
+ *
+ * @author USER
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char charArray[] ={ 'H','E','L','L','O','W','O','R','L','D','\0'};
+        char copyto[] =new char[20];
+        System.arraycopy(charArray,5,copyto,0,6);
+        System.out.println(new String(copyto));
+    }
+    
+}
